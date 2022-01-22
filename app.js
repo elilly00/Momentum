d148ed992d1ce2ee695f88bdce820e08c977223b
eliily00 <wjddms0700@gmail.com>
2022-01-22 22:04:22 +0900

    22.01.22

---
+++ app.js
@@ -0,0 +1,36 @@
+/* [Data Type] 
+    1. 숫자
+        1) Integer : full Number(정수)
+        2) float : ex) 2.5(실수)
+    2. 문자
+        Stirng        
+
+    [Variables : 변수]
+    variables can't have a space
+    
+    - 변수명 작성법 : camelCase(낙타 등 모양)
+        ex) veryLongVariableName
+    
+    1. constant (상수)
+        const : 재선언, 재할당 불가
+
+    2. let 
+ */
+console.log(); // print something in the console
+
+console.log(5 + 2);
+console.log(5 * 2);
+console.log(5 / 2);
+
+let a = 5;
+const b = 2;
+let myName = "elilly";
+
+console.log(a + b);
+console.log(a * b);
+console.log(a / b);
+console.log("hello " + myName);
+
+myName = "jeong";
+
+console.log("your name is " + myName);
